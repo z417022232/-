f64bb50d3c855c4caa219e22f29eab840e8c2d1d
z417022232 <55504528+z417022232@users.noreply.github.com>
2026-07-27 15:28:28 +0800

    Sync src/core/config.cpp from trainer core plan

--- src/core/config.cpp
+++ src/core/config.cpp
@@ -8,4 +8,17 @@ Config Config::load_default() {
     };
 }
 
+Config Config::from_args(int argc, char* argv[]) {
+    Config config = load_default();
+
+    for (int index = 1; index < argc; ++index) {
+        const std::string arg = argv[index];
+        if ((arg == "--target" || arg == "-t") && index + 1 < argc) {
+            config.target_process_name = argv[++index];
+        }
+    }
+
+    return config;
+}
+
 }  // namespace trainer
