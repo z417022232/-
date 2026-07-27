836d95f563b942c53cada2262d34c3f9e381e0d7
z417022232 <55504528+z417022232@users.noreply.github.com>
2026-07-27 15:28:54 +0800

    Sync src/platform/process.hpp from trainer core plan

--- src/platform/process.hpp
+++ src/platform/process.hpp
@@ -11,6 +11,35 @@ struct ProcessInfo {
     std::string name;
 };
 
+class ProcessHandle {
+public:
+    ProcessHandle() = default;
+    explicit ProcessHandle(ProcessInfo info);
+    ProcessHandle(const ProcessHandle&) = delete;
+    ProcessHandle& operator=(const ProcessHandle&) = delete;
+    ProcessHandle(ProcessHandle&& other) noexcept;
+    ProcessHandle& operator=(ProcessHandle&& other) noexcept;
+    ~ProcessHandle();
+
+    [[nodiscard]] bool is_open() const;
+    [[nodiscard]] const ProcessInfo& info() const;
+
+#ifdef _WIN32
+    [[nodiscard]] void* native_handle() const;
+#endif
+
+private:
+    void close();
+
+    ProcessInfo info_{};
+#ifdef _WIN32
+    void* handle_ = nullptr;
+#else
+    bool open_ = false;
+#endif
+};
+
 std::optional<ProcessInfo> find_process_by_name(const std::string& process_name);
+std::optional<ProcessHandle> open_process(const ProcessInfo& process);
 
 }  // namespace trainer
