3f8f31bfd505d3e2431781b5968238c724789c0c
z417022232 <55504528+z417022232@users.noreply.github.com>
2026-07-27 15:46:42 +0800

    Add config file fields

--- src/core/config.hpp
+++ src/core/config.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
+#include <map>
 #include <string>
 
 namespace trainer {
 
 struct Config {
     std::string target_process_name;
+    std::string log_path;
+    std::map<std::string, std::string> cheat_hotkeys;
 
     static Config load_default();
+    static Config from_file(const std::string& path);
     static Config from_args(int argc, char* argv[]);
 };
 
